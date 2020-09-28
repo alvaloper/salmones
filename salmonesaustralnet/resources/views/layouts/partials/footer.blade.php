da148fcb30cf7af7531fd350ce9752f28cb7719d
ingleidyespinoza <ing.leidyespinoza@gmail.com>
2020-09-27 22:43:28 -0400

    Update icons views

--- salmonesaustralnet/resources/views/layouts/partials/footer.blade.php
+++ salmonesaustralnet/resources/views/layouts/partials/footer.blade.php
@@ -5,5 +5,5 @@
         <a href="https://github.com/acacha/adminlte-laravel"></a><b>admin-lte-laravel</b></a>. {{ trans('adminlte_lang::message.descriptionpackage') }}
     </div> -->
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2015 <a href="http://acacha.org">Acacha.org</a>.</strong> {{ trans('adminlte_lang::message.createdby') }} <a href="http://acacha.org/sergitur">Sergi Tur Badenas</a>. y Modificado por <a href="https://twitter.com/@tarsicio_tic" target="_blank">@tarsicio_tic</a> Noviembre del 2016
+    <strong>Copyright &copy; 2020.</strong> {{ trans('adminlte_lang::message.createdby') }} <a href="http://salmonesaustral.net">salmonesaustral.net</a>
 </footer>
