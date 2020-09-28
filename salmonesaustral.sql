ed9de6e31b5bc37c415e2380c388696305fe81b9
ingleidyespinoza <ing.leidyespinoza@gmail.com>
2020-09-27 22:04:23 -0400

    Init Database SAustral

---
+++ salmonesaustral.sql
@@ -0,0 +1,348 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-09-2020 a las 04:03:46
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `salmonesaustral`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `migrations`
+--
+
+INSERT INTO `migrations` (`migration`, `batch`) VALUES
+('2014_10_12_000000_create_users_table', 1),
+('2014_10_12_100000_create_password_resets_table', 1),
+('2015_01_15_105324_create_roles_table', 1),
+('2015_01_15_114412_create_role_user_table', 1),
+('2015_01_26_115212_create_permissions_table', 1),
+('2015_01_26_115523_create_permission_role_table', 1),
+('2015_02_09_132439_create_permission_user_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `slug`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'crear.usuarios', 'Permiso Para Crear Usuario', '2020-09-28 01:33:04', '2020-09-28 01:33:04'),
+(2, 'eliminar.usuarios', 'Permiso Para Eliminar Usuario', '2020-09-28 01:33:04', '2020-09-28 01:33:04'),
+(3, 'ver.usuarios', 'Permiso Para Ver Usuario', '2020-09-28 01:33:04', '2020-09-28 01:33:04'),
+(4, 'modificar.usuarios', 'Permiso Para Modificar Usuario', '2020-09-28 01:33:04', '2020-09-28 01:33:04'),
+(5, 'crear.perfiles', 'Permiso Para Crear Perfiles', '2020-09-28 01:33:04', '2020-09-28 01:33:04'),
+(6, 'eliminar.perfiles', 'Permiso Para Eliminar Perfiles', '2020-09-28 01:33:04', '2020-09-28 01:33:04'),
+(7, 'ver.perfiles', 'Permiso Para Ver perfiles', '2020-09-28 01:33:05', '2020-09-28 01:33:05'),
+(8, 'modificar.perfiles', 'Permiso para Modificar perfiles', '2020-09-28 01:33:05', '2020-09-28 01:33:05'),
+(9, 'crear.permisos', 'Para Crear perfiles', '2020-09-28 01:33:05', '2020-09-28 01:33:05'),
+(10, 'eliminar.permisos', 'Para Eliminar Perfiles', '2020-09-28 01:33:05', '2020-09-28 01:33:05'),
+(11, 'ver.permisos', 'Para Ver Perfiles', '2020-09-28 01:33:05', '2020-09-28 01:33:05'),
+(12, 'modificar.permisos', 'Para Modificar Perfiles', '2020-09-28 01:33:05', '2020-09-28 01:33:05');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permission_role`
+--
+
+CREATE TABLE `permission_role` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `permission_id` int(10) UNSIGNED NOT NULL,
+  `role_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `permission_role`
+--
+
+INSERT INTO `permission_role` (`id`, `permission_id`, `role_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(2, 2, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(3, 3, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(4, 4, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(5, 5, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(6, 6, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(7, 7, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(8, 8, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(9, 9, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(10, 10, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(11, 11, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(12, 12, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permission_user`
+--
+
+CREATE TABLE `permission_user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `permission_id` int(10) UNSIGNED NOT NULL,
+  `role_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `permission_user`
+--
+
+INSERT INTO `permission_user` (`id`, `permission_id`, `role_id`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(2, 2, 1, 1, '2020-09-28 01:33:06', '2020-09-28 01:33:06'),
+(3, 3, 1, 1, '2020-09-28 01:33:07', '2020-09-28 01:33:07'),
+(4, 4, 1, 1, '2020-09-28 01:33:07', '2020-09-28 01:33:07'),
+(5, 5, 1, 1, '2020-09-28 01:33:07', '2020-09-28 01:33:07'),
+(6, 6, 1, 1, '2020-09-28 01:33:07', '2020-09-28 01:33:07'),
+(7, 7, 1, 1, '2020-09-28 01:33:07', '2020-09-28 01:33:07'),
+(8, 8, 1, 1, '2020-09-28 01:33:07', '2020-09-28 01:33:07'),
+(9, 9, 1, 1, '2020-09-28 01:33:07', '2020-09-28 01:33:07'),
+(10, 10, 1, 1, '2020-09-28 01:33:07', '2020-09-28 01:33:07'),
+(11, 11, 1, 1, '2020-09-28 01:33:07', '2020-09-28 01:33:07'),
+(12, 12, 1, 1, '2020-09-28 01:33:07', '2020-09-28 01:33:07');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `level` int(11) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id`, `slug`, `description`, `level`, `created_at`, `updated_at`) VALUES
+(1, 'root', 'Usuario Super Administrador', 1, '2020-09-28 01:33:05', '2020-09-28 01:33:05'),
+(2, 'admin', 'Usuario Administrador', 2, '2020-09-28 01:33:05', '2020-09-28 01:33:05');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `role_user`
+--
+
+CREATE TABLE `role_user` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `role_id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `role_user`
+--
+
+INSERT INTO `role_user` (`id`, `role_id`, `user_id`, `created_at`, `updated_at`) VALUES
+(2, 1, 1, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `foto` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `login` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  `fechainicio` date DEFAULT NULL,
+  `fechafin` date DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `foto`, `login`, `email`, `password`, `activo`, `fechainicio`, `fechafin`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Salmones Austral', 've-user8-128x128.jpg', 'Salmones Austral CTO', 'administrador@salmonesaustral.net', '$2y$10$4C4FqrB4.vQbnhEGCBqPeu0rIIphDk3evfgp4q5ta2FVMk5qWu/SC', 0, '2020-09-27', '2021-03-27', 'IlKzOAFif5lEJEnlVN8SljquhtC4NbQHJL8ZNw00D7bwB2Uu9q5ZOn46yTGl', '2020-09-28 01:33:05', '2020-09-28 01:40:08');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`),
+  ADD KEY `password_resets_token_index` (`token`);
+
+--
+-- Indices de la tabla `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `permissions_slug_unique` (`slug`);
+
+--
+-- Indices de la tabla `permission_role`
+--
+ALTER TABLE `permission_role`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `permission_role_permission_id_index` (`permission_id`),
+  ADD KEY `permission_role_role_id_index` (`role_id`);
+
+--
+-- Indices de la tabla `permission_user`
+--
+ALTER TABLE `permission_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `permission_user_permission_id_index` (`permission_id`),
+  ADD KEY `permission_user_role_id_index` (`role_id`),
+  ADD KEY `permission_user_user_id_index` (`user_id`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `roles_slug_unique` (`slug`);
+
+--
+-- Indices de la tabla `role_user`
+--
+ALTER TABLE `role_user`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_user_role_id_index` (`role_id`),
+  ADD KEY `role_user_user_id_index` (`user_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_login_unique` (`login`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT de la tabla `permission_role`
+--
+ALTER TABLE `permission_role`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT de la tabla `permission_user`
+--
+ALTER TABLE `permission_user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `role_user`
+--
+ALTER TABLE `role_user`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `permission_role`
+--
+ALTER TABLE `permission_role`
+  ADD CONSTRAINT `permission_role_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `permission_role_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `permission_user`
+--
+ALTER TABLE `permission_user`
+  ADD CONSTRAINT `permission_user_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `permission_user_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `permission_user_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `role_user`
+--
+ALTER TABLE `role_user`
+  ADD CONSTRAINT `role_user_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `role_user_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
