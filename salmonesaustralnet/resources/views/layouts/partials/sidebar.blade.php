c2e754f63740dccca3e4466fb515cfca0347c499
ingleidyespinoza <ing.leidyespinoza@gmail.com>
2020-09-28 13:54:55 -0400

    Update Views Sidebar

--- salmonesaustralnet/resources/views/layouts/partials/sidebar.blade.php
+++ salmonesaustralnet/resources/views/layouts/partials/sidebar.blade.php
@@ -12,18 +12,17 @@
                 </div>
                 <div class="pull-left info">
                     <p>{{ Auth::user()->name }}</p>
-                    <!-- Status -->
-                    <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('adminlte_lang::message.online') }}</a>
                 </div>
             </div>
         @endif
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
-            <li class="header">{{ trans('adminlte_lang::message.header') }}</li>
+            <li class="header">Seleccione una opción:</li>
             <!-- Optionally, you can add icons to the links -->
             <li class="active"><a href="{{ url('home') }}"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.home') }}</span></a></li>
             <li><a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.anotherlink') }}</span></a></li>
+            <li><a href="{{ url('admin/users') }}"><i class='fa fa-link'></i> <span>Seguridad</span></a></li>
             <li class="treeview">
                 <a href="#"><i class='fa fa-link'></i> <span>{{ trans('adminlte_lang::message.multilevel') }}</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
