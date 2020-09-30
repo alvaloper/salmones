de0354450000e031c180a4d6f76c0d9349ea6468
ingleidyespinoza <ing.leidyespinoza@gmail.com>
2020-09-30 19:01:51 -0400

    Update Views MainHeader and Delete images --> public/img

--- salmonesaustralnet/resources/views/layouts/partials/mainheader.blade.php
+++ salmonesaustralnet/resources/views/layouts/partials/mainheader.blade.php
@@ -28,14 +28,14 @@
                         <!-- Menu Toggle Button -->
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             <!-- The user image in the navbar-->
-                            <img src="{{asset('/img/avatar04.png')}}" class="user-image" alt="User Image"/>
+                            <img src="{{asset('img/logoavatar.png')}}" class="user-image" alt="User Image"/>
                             <!-- hidden-xs hides the username on small devices so only the image appears. -->
                             <span class="hidden-xs">{{ Auth::user()->name }}</span>
                         </a>
                         <ul class="dropdown-menu">
                             <!-- The user image in the menu -->
                             <li class="user-header">
-                                <img src="{{asset('/img/avatar04.png')}}" class="img-circle" alt="User Image" />
+                                <img src="{{asset('img/logoavatar.png')}}" class="img-circle" alt="User Image" />
                                 <p>
                                     {{ Auth::user()->name }}
                                 </p>
@@ -43,9 +43,7 @@
 
                             <!-- Menu Footer-->
                             <li class="user-footer">
-                                <div class="pull-left">
-                                    <a href="#" class="btn btn-default btn-flat">{{ trans('adminlte_lang::message.profile') }}</a>
-                                </div>
+   
                                 <div class="pull-right">
                                     <a href="{{ url('/logout') }}" class="btn btn-default btn-flat">{{ trans('adminlte_lang::message.signout') }}</a>
                                 </div>
