5437097f1be1e89ce2805fe70428f25d10f6257a
ingleidyespinoza <ing.leidyespinoza@gmail.com>
2020-10-06 16:38:44 -0400

    Update Migrations and Seeder of Users

--- salmonesaustralnet/database/seeds/UsersTableSeeder.php
+++ salmonesaustralnet/database/seeds/UsersTableSeeder.php
@@ -13,89 +13,89 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::create(array(
-            'name'           => 'Salmones Austral',
-            'lastname'       => 'Salmones Austral CTO',
+            'name'           => 'Salmones',
+            'lastname'       => 'Austral',
             'email'          => 'administrador@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
 
         User::create(array(
-            'name'           => 'María Morales',
-            'lastname'       => 'Salmones Austral EDIT',
+            'name'           => 'María',
+            'lastname'       => 'Fernández',
             'email'          => 'editor@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
 
         User::create(array(
-            'name'           => 'Luis Espinoza',
-            'lastname'       => 'Root 1',
+            'name'           => 'Luis',
+            'lastname'       => 'Espinoza',
             'email'          => 'espinozalj@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
 
         User::create(array(
-            'name'           => 'Leidy Espinoza',
-            'lastname'       => 'Root 2',
+            'name'           => 'Leidy',
+            'lastname'       => 'Espinoza',
             'email'          => 'espinozalb@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
 
         User::create(array(
-            'name'           => 'Itala Espinoza',
-            'lastname'       => 'Admin 1',
+            'name'           => 'Itala',
+            'lastname'       => 'Espinoza',
             'email'          => 'espinozai@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
 
         User::create(array(
-            'name'           => 'Jose Espinoza',
-            'lastname'       => 'Admin 2',
+            'name'           => 'Jose',
+            'lastname'       => 'Espinoza',
             'email'          => 'espinozaj@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
 
         User::create(array(
-            'name'           => 'Leidy Alvarado',
-            'lastname'       => 'Admin 3',
+            'name'           => 'Leidy',
+            'lastname'       => 'Alvarado',
             'email'          => 'alvaradol@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
 
         User::create(array(
-            'name'           => 'Luis Alvarado',
-            'lastname'       => 'Admin 4',
+            'name'           => 'Luis',
+            'lastname'       => 'Alvarado',
             'email'          => 'alvaradolj@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
 
         User::create(array(
-            'name'           => 'Itala Alvarado',
-            'lastname'       => 'Admin 5',
+            'name'           => 'Itala',
+            'lastname'       => 'Alvarado',
             'email'          => 'alvaradoi@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
 
         User::create(array(
-            'name'           => 'Luis Pulido',
-            'lastname'       => 'Admin 6',
-            'email'          => 'pulidol@salmonesaustral.net',
+            'name'           => 'Luis',
+            'lastname'       => 'Pérez',
+            'email'          => 'perezl@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
 
         User::create(array(
-            'name'           => 'Jose Pulido',
-            'lastname'       => 'Admin 7',
-            'email'          => 'pulidoj@salmonesaustral.net',
+            'name'           => 'Jose',
+            'lastname'       => 'Marquéz',
+            'email'          => 'marquezj@salmonesaustral.net',
             'password'       => Hash::make('123456'),
             'remember_token' => str_random(100)
         ));
