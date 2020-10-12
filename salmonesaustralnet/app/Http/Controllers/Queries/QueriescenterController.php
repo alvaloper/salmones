7ff4de6d6de4f59ff56a5b69cea20b6c06119aae
ingleidyespinoza <ing.leidyespinoza@gmail.com>
2020-10-11 23:04:47 -0400

    Update Controllers Center and Fault

--- salmonesaustralnet/app/Http/Controllers/Queries/QueriescenterController.php
+++ salmonesaustralnet/app/Http/Controllers/Queries/QueriescenterController.php
@@ -4,10 +4,48 @@ namespace App\Http\Controllers\Queries;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
+use App\Models\Seguridad\Permission;
+use App\Models\Seguridad\PermissionRole;
+use App\Models\Seguridad\Role;
+use App\User;
+use App\maritimerecords;
+use App\centers;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Validation;
+use App\Http\Requests\Maritimerecords\MaritimerecordsNewRequest;
+use App\Http\Requests\Maritimerecords\MaritimerecordsUpdateRequest;
+use App\Http\Requests;
 
 class QueriescenterController extends Controller
 {
-    //
+     public function __construct()
+    {
+
+        $this->middleware('auth');
+
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+
+        $maritimerecords = maritimerecords::Paginate(5);
+        $centersx = centers::all();
+        $userc = User::all();
+        return view('maritimerecords.index', array('maritimerecords'=> $maritimerecords, 'centersx' => $centersx, 'userc'=> $userc));
+
+    }
+
+    public function search(Request $request){
+
+
+    }
 }
--- salmonesaustralnet/app/Http/Controllers/Queries/QueriesfaultController.php
+++ salmonesaustralnet/app/Http/Controllers/Queries/QueriesfaultController.php
@@ -4,10 +4,48 @@ namespace App\Http\Controllers\Queries;
 
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
+
+use App\Models\Seguridad\Permission;
+use App\Models\Seguridad\PermissionRole;
+use App\Models\Seguridad\Role;
+use App\User;
+use App\faultrecords;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Validation;
+use App\Http\Requests\Faultrecords\FaultrecordsNewRequest;
+use App\Http\Requests\Faultrecords\FaultrecordsUpdateRequest;
+use App\Http\Requests;
 
 class QueriesfaultController extends Controller
 {
-    //
+	public function __construct()
+    {
+
+        $this->middleware('auth');
+
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+
+        $faultrecords = faultrecords::Paginate(5);
+
+        $userc = User::all();
+        return view('faultrecords.index', array('faultrecords'=> $faultrecords, 'userc'=> $userc));
+
+    }
+
+
+    public function search(Request $request){
+
+    }
 }
