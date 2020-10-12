c2309067dbc2d2f270ef9b791d1f5217b5208f54
ingleidyespinoza <ing.leidyespinoza@gmail.com>
2020-10-11 21:53:01 -0400

    Create Views Queries -> Centers and Faults

---
+++ salmonesaustralnet/resources/views/queries/faultsrecords/index.blade.php
@@ -0,0 +1,19 @@
+@extends('faultrecords.app')
+
+@section('htmlheader_title')
+    Fallas presentadas en el area de Control de Monitoreo
+@endsection
+
+
+@section('main-content')
+    @if(Auth::check() && (Auth::user()->isRole('root') || Auth::user()->isRole('admin')))
+       @include('flash::message')
+        <a class="btn btn-primary pull-right" href="{{ url('admin/faultrecords/create') }}" data-toggle="tooltip" title="Nueva Eventualidad" role="button">Nueva Eventualidad</a>
+        <br><br>
+    @include('faultrecords.partials.table')
+    @else
+        <div class="panel-body">
+            {{ trans('adminlte_lang::message.permiso') }}
+        </div>
+    @endif
+@endsection
