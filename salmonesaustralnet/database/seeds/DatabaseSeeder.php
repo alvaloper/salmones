20dc18dd72d1003ab23bdd0c0afa6524532a7cff
ingleidyespinoza <ing.leidyespinoza@gmail.com>
2020-10-14 19:22:54 -0400

    Update DatabaseSeeder

--- salmonesaustralnet/database/seeds/DatabaseSeeder.php
+++ salmonesaustralnet/database/seeds/DatabaseSeeder.php
@@ -11,18 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        /**
-         * El presente Seeder, nos permite inicializar las tablas de Rol, Permisos, Usuarios
-         * con sus respectivos detalles, para que al inicio de la instalación se pueda
-         * configurar de forma correcta la seguridad de la aplicación, tenemos 3 tablas maestras
-         * y tres tablas detalles.
-         */
-        //$this->call(PermissionTableSeeder::class);
+        $this->call(PermissionTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(RoleUserTableSeeder::class);
-        //$this->call(PermissionRoleTableSeeder::class);
-        //$this->call(PermissionUserTableSeeder::class);
+        $this->call(PermissionRoleTableSeeder::class);
+        $this->call(PermissionUserTableSeeder::class);
         $this->call(PlantsTableSeeder::class);
         $this->call(CentersTableSeeder::class);
         $this->call(PlantrecordsTableSeeder::class);
