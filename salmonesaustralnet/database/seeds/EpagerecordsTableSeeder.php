a708c2d90110b745ae2495d812c6694cb334fc38
ingleidyespinoza <ing.leidyespinoza@gmail.com>
2020-11-24 17:40:33 -0400

    Update Seeds EpageRecords

--- salmonesaustralnet/database/seeds/EpagerecordsTableSeeder.php
+++ salmonesaustralnet/database/seeds/EpagerecordsTableSeeder.php
@@ -28,6 +28,57 @@ class EpagerecordsTableSeeder extends Seeder
 			    'updated_at'  		  => \Carbon\Carbon::now(),
 
         ]);
+
+        DB::table('epagerecords')->insert([
+
+          'epagetitle'        => 'Incendio en Grúa 2',
+          'epagedate'         => \Carbon\Carbon::now(),
+          'epagehour'         => \Carbon\Carbon::now(),
+          'eventepage'        => 'Incendio en la Grúa 2. Fue controlado por el equipo de planta.',
+          'actionseventepage' => 'No se detallan',
+          'file1'             => 'nonpicture.jpg',
+          'file2'             => 'nonpicture.jpg',
+          'file3'             => 'nonpicture.jpg',
+          'file4'             => 'nonpicture.jpg',
+          'user_id'           => '1',
+          'created_at'        => \Carbon\Carbon::now(),
+          'updated_at'        => \Carbon\Carbon::now(),
+
+        ]);
+
+        DB::table('epagerecords')->insert([
+
+          'epagetitle'        => 'Sismo de baja intensidad',
+          'epagedate'         => \Carbon\Carbon::now(),
+          'epagehour'         => \Carbon\Carbon::now(),
+          'eventepage'        => 'Sismo de baja intensidad afecta operaciones en planta de procesos.',
+          'actionseventepage' => 'No se detallan',
+          'file1'             => 'nonpicture.jpg',
+          'file2'             => 'nonpicture.jpg',
+          'file3'             => 'nonpicture.jpg',
+          'file4'             => 'nonpicture.jpg',
+          'user_id'           => '2',
+          'created_at'        => \Carbon\Carbon::now(),
+          'updated_at'        => \Carbon\Carbon::now(),
+
+        ]);
+
+        DB::table('epagerecords')->insert([
+
+          'epagetitle'        => 'Tormenta Eléctrica',
+          'epagedate'         => \Carbon\Carbon::now(),
+          'epagehour'         => \Carbon\Carbon::now(),
+          'eventepage'        => 'Tormenta eléctrica produce fuerte corriente de vientos afectando el techo del Anden 1.',
+          'actionseventepage' => 'No se detallan',
+          'file1'             => 'nonpicture.jpg',
+          'file2'             => 'nonpicture.jpg',
+          'file3'             => 'nonpicture.jpg',
+          'file4'             => 'nonpicture.jpg',
+          'user_id'           => '1',
+          'created_at'        => \Carbon\Carbon::now(),
+          'updated_at'        => \Carbon\Carbon::now(),
+
+        ]);
         
     }
 }
